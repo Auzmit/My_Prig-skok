811f08b51992f0f8e8f2e92d6d8fe61a3473e082
Auzmit <atrudkov@mail.ru>
2024-06-21 01:55:47 +0300

    -double platforms with y=y; score; 3 failed experiments

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let context;
 let skokerWidth = 46;
 let skokerHeight = 46;
 let skokerX = boardWidth/2 - skokerWidth/2;
-let skokerY = boardHeight*7/8 - skokerHeight;
+let skokerY = boardHeight*7/8 - skokerHeight*2;
 let skokerLeftImage;
 let skokerRightImage;
 let skoker = {
@@ -19,21 +19,21 @@ let skoker = {
   height: skokerHeight
 }
 
-
 // physics
 let velocityX = 0;
-let velocityY = 0;
-let initialVelocityY = -9;
+let velocityY = -10;
+let initialVelocityY = -7.5;
 let gravity = 0.4;
 
 // platforms
 let arrPlatform = [];
-let platformImageWidth = 1200;
+let platformImageWidth = 1000;
 let platformImageHeight = 336;
 let coeffImageToPlatform = 7;
 let platformWidth = platformImageWidth/coeffImageToPlatform;
 let platformHeight = platformImageHeight/coeffImageToPlatform;
 let platformImage;
+let newPlatformCounter = 0;
 
 // clouds
 // let arrClouds = [];
@@ -78,13 +78,14 @@ window.onload = function() {
   //   context.drawImage(skoker.image, skoker.x, skoker.y, skoker.width, skoker.height);
   // }
 
-  velocityY = initialVelocityY;
+  // velocityY = initialVelocityY;
   placePlatforms();
   requestAnimationFrame(update);
   document.addEventListener('keydown', moveSkoker);
 }
 
 function update() {
+  // console.log(velocityY);
   requestAnimationFrame(update);
   context.clearRect(0, 0, board.width, board.height);
 
@@ -100,40 +101,59 @@ function update() {
   skoker.y += velocityY;
   context.drawImage(skoker.image, skoker.x, skoker.y, skoker.width, skoker.height);
 
-  // platforms
+  // platforms and velocityY
   for (let i = 0; i < arrPlatform.length; i++) {
     let platform = arrPlatform[i];
-    if (i !== 0) {
-      let previousPlatform = arrPlatform[i-1];
-    }
+    // if (i !== 0) {
+    //   let previousPlatform = arrPlatform[i-1];
+    // }
     if (velocityY < 0 && skoker.y < boardHeight*3/4) {
       platform.y -= initialVelocityY*0.8; // drop platform down
     }
     if (detectCollision(skoker, platform) && velocityY >= 0) {
       velocityY = initialVelocityY; // jump from the platform
+      // for (let i = 0; i < arrPlatform.length; i++) {
+      //   console.log(arrPlatform[i]);  
+      // }
+      // console.log('jump');
+      // velocityY = initialVelocityY - 3; // jump from the platform
     }
-    if (typeof previousPlatform !== 'undefined') {
-      if (platform.y - previousPlatform.y > 50) {
-        context.drawImage(platform.image, platform.x, platform.y, platform.width, platform.height);
-      }
-      // else console.log(platform.y - previousPlatform.y);
-    } else {
-      context.drawImage(platform.image, platform.x, platform.y, platform.width, platform.height);
-    }
+    context.drawImage(platform.image, platform.x, platform.y, platform.width, platform.height);
+    
+    // if (typeof previousPlatform !== 'undefined') {
+    //   if (platform.y - previousPlatform.y > 50) {
+    //     context.drawImage(platform.image, platform.x, platform.y, platform.width, platform.height);
+    //   }
+    //   // else console.log(platform.y - previousPlatform.y);
+    // } else {
+    // }
   }
 
-  while (arrPlatform.length > 0 && arrPlatform[0].y >= boardHeight) {
+  while (arrPlatform.length > 0 && arrPlatform[0].y >= boardHeight+10) {
+    // for (let i = 0; i < arrPlatform.length; i++) {
+    //   console.log(arrPlatform[i]);  
+    // }
+    // console.log('shift');
+
     arrPlatform.shift();
     newPlatform();
+    newPlatformCounter += 1;
+    // console.log(newPlatformCounter);
   }
+
+  // score
+  // updateScore();
+  context.fillStyle = 'black';
+  context.font = 'bold 50px Sans-Serif';
+  context.fillText(score, 5, 42);
 }
 
 function moveSkoker(e) {
   if (e.code == 'ArrowRight' || e.code == 'KeyD') {
-    velocityX = 4;
+    velocityX = 5;
     skoker.image = skokerRightImage;
   } else if (e.code == 'ArrowLeft' || e.code == 'KeyA') {
-    velocityX = -4;
+    velocityX = -5;
     skoker.image = skokerLeftImage;
   }
 }
@@ -143,15 +163,17 @@ function moveSkoker(e) {
 // }
 
 function placePlatforms() {
-  arrPlatform = [];
+  // arrPlatform = [];
   // platformImage.src = arrClouds[randomIntFromInterval(1, 6)];
 
   // 1-st (starting) platform
   let platform = {
     image: platformImage,
-    x: boardWidth/2,
+    x: boardWidth,
+    // x: 0,
     y: boardHeight - 70,
     width: platformWidth,
+    // width: boardWidth,
     height: platformHeight
   }
 
@@ -167,19 +189,23 @@ function placePlatforms() {
 
   // arrPlatform.push(platform);
 
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 6; i++) {
     let randomX = Math.floor(Math.random() * boardWidth*3/4);
     // platformImage.src = arrClouds[randomIntFromInterval(1, 6)];
     let platform = {
       image: platformImage,
       x: randomX,
-      y: boardHeight - 160*i - 150,
+      y: boardHeight - 160*i,
       // y: boardHeight - 300,
       width: platformWidth,
       height: platformHeight
     }
   
     arrPlatform.push(platform);
+    // for (let i = 0; i < arrPlatform.length; i++) {
+    //   console.log(arrPlatform[i]);  
+    // }
+    // console.log('add more');
   }
 }
 
@@ -194,8 +220,26 @@ function newPlatform() {
       width: platformWidth,
       height: platformHeight
     }
-  
-    arrPlatform.push(platform);
+    if (arrPlatform[arrPlatform.length - 1].y !== platform.y) {
+      arrPlatform.push(platform);
+    }
+    // console.log(arrPlatform[arrPlatform.length - 1].y, platform.y);
+    // console.log(arrPlatform[arrPlatform.length - 1].y - platform.y);
+    // console.log('-');
+    // if ((arrPlatform[arrPlatform.length - 1].y - platform.y < 55
+    //   && (arrPlatform[arrPlatform.length - 1].y - platform.y > -55))
+    // ) {
+    //   platform.y -= platformHeight*1;
+    // }
+    // console.log(arrPlatform[arrPlatform.length - 1].y, platform.y);
+    // console.log(arrPlatform[arrPlatform.length - 1].y - platform.y);
+    // arrPlatform.push(platform);
+    // for (let i = 0; i < arrPlatform.length; i++) {
+    //   console.log(arrPlatform[i]);  
+    // }
+    // console.log('add 1');
+    // console.log(arrPlatform[arrPlatform.length - 1].y, platform.y);
+    score += 1;
 }
 
 function detectCollision(a, b) {
@@ -204,3 +248,8 @@ function detectCollision(a, b) {
          a.y < b.y + b.height &&  // on y: a's Top Left corner < b's Bottom Right corner
          a.y + a.height > b.y;    // on y: a's Top Right corner < b's Bottom Left corner
 }
+
+// function updateScore() {
+//   let point = Math.floor(50 * Math.random());
+//   score += 1;
+// }
