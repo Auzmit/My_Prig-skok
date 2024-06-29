ab19ab7bae2b8211caee4de046008ae770afbcc6
Andrew Trudkov <atrudkov@mail.ru>
2024-06-29 23:34:13 +0300

    1) begin coords for skoker & 1-st plat. 2) f newplat in placePlats. 3) some physics moment

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
 // 'newPlatform' вставить в 'placePlatforms'
 
 import detectCollision from './detectCollision.js';
+import randomInteger from './randomInteger.js';
 
 // board init
 let board;
@@ -13,11 +14,17 @@ let boardWidth = 600;
 let boardHeight = 800;
 let context;
 
+// physics init
+let velocityX = 0;
+let velocityY = 0;
+let initialVelocityY = -8;
+let gravity = 0.4;
+
 // skoker init
 let skokerWidth = 46;
 let skokerHeight = 46;
 let skokerX = boardWidth/2 - skokerWidth/2;
-let skokerY = boardHeight*7/8 - skokerHeight*2;
+let skokerY = boardHeight*0.9 - skokerHeight;
 let skokerLeftImage;
 let skokerRightImage;
 let skoker = {
@@ -28,19 +35,13 @@ let skoker = {
   height: skokerHeight
 }
 
-// physics init
-let velocityX = 0;
-let velocityY = 0;
-let initialVelocityY = -8;
-let gravity = 0.4;
-
 // platforms init
 let arrPlatform = [];
-let platformImageWidth = 1000;
-let platformImageHeight = 336;
-let coeffImageToPlatform = 7;
-let platformWidth = platformImageWidth/coeffImageToPlatform;
-let platformHeight = platformImageHeight/coeffImageToPlatform;
+const platformImageWidth = 1000;
+const platformImageHeight = 336;
+const coeffImageToPlatform = 7;
+const platformWidth = platformImageWidth/coeffImageToPlatform;
+const platformHeight = platformImageHeight/coeffImageToPlatform;
 let platformImage;
 let arrPlatformImages = [];
 for (let i = 1; i <= 6; i++) {
@@ -66,49 +67,53 @@ window.onload = function() {
   skoker.image = skokerRightImage;
   skokerRightImage.onload = function() {
     context.drawImage(skoker.image, skoker.x, skoker.y, skoker.width, skoker.height);
-  }
+  };
 
   velocityY = initialVelocityY;
   placePlatforms(arrPlatformImages);
   requestAnimationFrame(update);
   document.addEventListener('keydown', moveSkoker);
-}
+};
 
 function update() {
   requestAnimationFrame(update);
   context.clearRect(0, 0, board.width, board.height);
 
   // draw skoker
+  velocityY += gravity;
+  skoker.y += velocityY;
   skoker.x += velocityX;
+  // jump from side to side of the screen
   if (skoker.x > board.width) {
     skoker.x = 0;
   } else if (skoker.x + skoker.width < 0) {
     skoker.x = board.width;
-  }
-
-  velocityY += gravity;
-  skoker.y += velocityY;
+  };
+  //
   context.drawImage(skoker.image, skoker.x, skoker.y, skoker.width, skoker.height);
 
   // platforms and velocityY
   for (let i = 0; i < arrPlatform.length; i++) {
     let platform = arrPlatform[i];
-    if (velocityY < 0 && skoker.y < boardHeight*3/4) {
-      platform.y -= initialVelocityY*0.8; // drop all platforms little bit down
+    if (velocityY < 0 && skoker.y < boardHeight*0.74) {
+      platform.y -= initialVelocityY*0.7; // shift all plats little down
+      for (let i = 0; i < arrPlatform.length; i ++) {
+        console.log(arrPlatform[i]);
+      };
     }
     if (detectCollision(skoker, platform) && velocityY >= 0) {
       velocityY = initialVelocityY; // jump from the platform
     }
     context.drawImage(platform.image, platform.x, platform.y, platform.width, platform.height);
-  }
+  };
 
-  while (arrPlatform.length > 0 && arrPlatform[0].y >= boardHeight+10) {
+  while (arrPlatform.length > 0 && arrPlatform[0].y >= boardHeight) {
     arrPlatform.shift();
     newPlatform();
     score += 1;
-  }
+  };
 
-  // score
+  // score draw
   context.fillStyle = 'black';
   context.font = 'bold 50px Sans-Serif';
   context.fillText(score, 5, 42);
@@ -121,16 +126,10 @@ function moveSkoker(event) {
   } else if (event.code == 'ArrowLeft' || event.code == 'KeyA') {
     velocityX = -5;
     skoker.image = skokerLeftImage;
-  }
-}
-
-function randomInteger(min, max) { // min and max included 
-  let randomNumber = Math.random() * (max - min + 1) + min;
-  return Math.floor(randomNumber);
-}
+  };
+};
 
 function placePlatforms() {
-
   let platformImage = new Image();
   platformImage.src = arrPlatformImages[
     randomInteger(0, arrPlatformImages.length - 1)];
@@ -138,33 +137,24 @@ function placePlatforms() {
   // 1-st (starting) platform
   let platform = {
     image: platformImage,
-    x: boardWidth/2,
-    // x: 0,
-    y: boardHeight - 70,
+    x: boardWidth/2 - platformWidth/2,
+    y: boardHeight - platformHeight,
     width: platformWidth,
     height: platformHeight
-  }
-
+  };
   arrPlatform.push(platform);
 
-  for (let i = 0; i < 6; i++) {
-    let randomX = Math.floor(Math.random() * boardWidth*3/4);
-    let platform = {
-      image: platformImage,
-      x: randomX,
-      y: boardHeight - 160*i,
-      width: platformWidth,
-      height: platformHeight
-    }
-  
-    arrPlatform.push(platform);
-  }
-}
+  while (arrPlatform[arrPlatform.length - 1].y > 0) {
+    newPlatform();
+  };
+};
 
 function newPlatform() {
-  let coeffWidthPadding = boardWidth * 0.02;
-  let randomX = randomInteger(0 + boardWidth * coeffWidthPadding,
-    boardWidth * coeffWidthPadding - platformWidth);
+  // X-coord randoming with little indent on left & right
+  let coeffWidthPadding = 0.02;
+  let widthPadding = boardWidth * coeffWidthPadding;
+  let randomX = randomInteger(widthPadding,
+    boardWidth - widthPadding - platformWidth);
 
   let platformImage = new Image();
   platformImage.src = arrPlatformImages[
@@ -173,11 +163,11 @@ function newPlatform() {
   let platform = {
     image: platformImage,
     x: randomX,
-    y: -platformHeight,
+    y: arrPlatform[arrPlatform.length - 1].y - 100 - randomInteger(0, 60),
+    // y: arrPlatform[arrPlatform.length - 1].y - 160,
     width: platformWidth,
     height: platformHeight
-  }
-  if (arrPlatform[arrPlatform.length - 1].y !== platform.y) {
-    arrPlatform.push(platform);
-  }
-}
+  };
+
+  arrPlatform.push(platform);
+};
